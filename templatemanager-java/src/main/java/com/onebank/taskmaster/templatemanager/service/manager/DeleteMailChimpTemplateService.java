c4b16eeb4907f7c7aab519301ffa10df658571ea
Christian Rivera <crivera2093@gmail.com>
2025-01-11 19:35:48 -0500

    feat(templatemanager-java): update components for delete mailchimp template flow.

--- templatemanager-java/src/main/java/com/onebank/taskmaster/templatemanager/service/manager/DeleteMailChimpTemplateService.java
+++ templatemanager-java/src/main/java/com/onebank/taskmaster/templatemanager/service/manager/DeleteMailChimpTemplateService.java
@@ -1,13 +1,28 @@
 package com.onebank.taskmaster.templatemanager.service.manager;
 
+import com.onebank.taskmaster.templatemanager.email.model.mailchimp.GetTemplateInfoRequest;
+import com.onebank.taskmaster.templatemanager.email.model.mailchimp.TemplateDetailResponse;
+import com.onebank.taskmaster.templatemanager.email.provider.mailchimp.MailChimpClient;
+import com.onebank.taskmaster.templatemanager.exception.ResourceNotFoundException;
 import com.onebank.taskmaster.templatemanager.model.ActionTemplateDto;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 
 @RequiredArgsConstructor
+@Slf4j
 public class DeleteMailChimpTemplateService extends DeleteTemplate {
+    private final MailChimpClient mailChimpClient;
 
     @Override
     void execute(ActionTemplateDto actionTemplateDto) {
-
+        TemplateDetailResponse templateDetailResponse;
+        try {
+            log.debug("Deleting Mailchimp template [{}]", actionTemplateDto.getName());
+            templateDetailResponse = mailChimpClient.deleteTemplate(new GetTemplateInfoRequest(actionTemplateDto.getName()));
+            log.debug("Mailchimp template [{}] deleted", templateDetailResponse.getName());
+        } catch (ResourceNotFoundException e) {
+            log.debug("Mailchimp template [{}] was not found", actionTemplateDto.getName());
+        }
+        log.debug("Action completed successfully for [{}] template", actionTemplateDto.getName());
     }
 }
