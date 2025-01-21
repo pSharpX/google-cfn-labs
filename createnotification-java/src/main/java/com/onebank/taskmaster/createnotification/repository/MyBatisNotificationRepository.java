a4148b20e5f3f0d828b11b7c40a6f3c0b00f4178
Christian Rivera <crivera2093@gmail.com>
2025-01-21 14:43:50 -0500

    feat(createnotification): update create-notification operation.

--- createnotification-java/src/main/java/com/onebank/taskmaster/createnotification/repository/MyBatisNotificationRepository.java
+++ createnotification-java/src/main/java/com/onebank/taskmaster/createnotification/repository/MyBatisNotificationRepository.java
@@ -1,9 +1,15 @@
 package com.onebank.taskmaster.createnotification.repository;
 
 import com.onebank.taskmaster.createnotification.entity.NotificationEntity;
+import com.onebank.taskmaster.createnotification.exception.ResourceNotFoundException;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.ibatis.session.SqlSession;
 import org.apache.ibatis.session.SqlSessionFactory;
+
+import java.util.Optional;
+
+import static com.onebank.taskmaster.createnotification.exception.utils.ExceptionConstantsUtils.NOT_FOUND;
 
 @Slf4j
 @RequiredArgsConstructor
@@ -11,7 +17,25 @@ public class MyBatisNotificationRepository implements NotificationRepository {
     private final SqlSessionFactory sqlSessionFactory;
 
     @Override
+    public Optional<NotificationEntity> findById(Long id) {
+        try (SqlSession sqlSession = sqlSessionFactory.openSession()) {
+            return sqlSession.getMapper(NotificationRepository.class).findById(id);
+        }
+    }
+
+    @Override
     public NotificationEntity save(NotificationEntity entity) {
-        return null;
+        try (SqlSession sqlSession = sqlSessionFactory.openSession()) {
+            NotificationRepository repository = sqlSession.getMapper(NotificationRepository.class);
+            Long id = repository.create(entity);
+            return  repository.findById(id).orElseThrow(() -> new ResourceNotFoundException(NOT_FOUND, "notification record not found"));
+        }
+    }
+
+    @Override
+    public Long create(NotificationEntity entity) {
+        try (SqlSession sqlSession = sqlSessionFactory.openSession()) {
+            return sqlSession.getMapper(NotificationRepository.class).create(entity);
+        }
     }
 }
