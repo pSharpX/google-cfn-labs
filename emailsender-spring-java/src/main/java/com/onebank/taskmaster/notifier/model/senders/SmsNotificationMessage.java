1a2cb88abcc971e848cca0e8dbea9b6eb979dcd8
Christian Rivera <crivera2093@gmail.com>
2025-01-27 11:17:05 -0500

    feat(notification): update SmsNotificationMessage model.

--- emailsender-spring-java/src/main/java/com/onebank/taskmaster/notifier/model/senders/SmsNotificationMessage.java
+++ emailsender-spring-java/src/main/java/com/onebank/taskmaster/notifier/model/senders/SmsNotificationMessage.java
@@ -4,12 +4,17 @@ import com.onebank.taskmaster.notifier.model.NotificationChannel;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.util.HashMap;
+import java.util.Map;
+
 @Getter
 @Setter
 public class SmsNotificationMessage extends NotificationMessage {
     private String recipientPhoneNumber;
     private String senderName;
     private boolean isUnicode;
+    private Map<String, String> vars = new HashMap<>();
+    private String templateName;
 
     public SmsNotificationMessage() {
         super(NotificationChannel.SMS);
