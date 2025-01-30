b6ce4ae76d4e557a8d7c52e4a951006317916a76
Christian Rivera <crivera2093@gmail.com>
2025-01-30 18:19:56 -0500

    feat(notification): update field name in sms-type notification model.

--- emailsender-spring-java/src/main/java/com/onebank/taskmaster/notifier/model/senders/SmsNotificationMessage.java
+++ emailsender-spring-java/src/main/java/com/onebank/taskmaster/notifier/model/senders/SmsNotificationMessage.java
@@ -11,7 +11,7 @@ import java.util.Map;
 @Setter
 public class SmsNotificationMessage extends NotificationMessage {
     private String recipientPhoneNumber;
-    private String senderName;
+    private String recipientName;
     private boolean isUnicode;
     private Map<String, String> vars = new HashMap<>();
     private String templateName;
